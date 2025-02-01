093ca4b0ceac2ec0603181e58ff4f8e153d7f979
Bryan <bryananthony763@gmail.com>
2025-02-01 11:17:27 -0500

    fix bugs

--- shared/config/awsConfig.js
+++ shared/config/awsConfig.js
@@ -2,7 +2,7 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { S3Client } = require("@aws-sdk/client-s3");
 
 const dynamoDB = new DynamoDBClient({
-    region: process.env.AWS_REGION || "us-east-1",
+    region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
@@ -10,11 +10,13 @@ const dynamoDB = new DynamoDBClient({
 });
 
 const s3 = new S3Client({
-    region: process.env.AWS_REGION || "us-east-1",
+    region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
 });
 
-module.exports = { dynamoDB, s3 };
+const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME; // Nombre del bucket en S3
+
+module.exports = { dynamoDB, s3, BUCKET_NAME };
